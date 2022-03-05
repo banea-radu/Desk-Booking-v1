3157c47c76795686c6957ee294395419369a37e3
banea-radu <100232231+banea-radu@users.noreply.github.com>
2022-03-05 19:47:32 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -102,14 +102,14 @@ function getFromDB() {
     onValue(ref(database, 'Bookings/' + yyyy + '/' + mm + '/' + dd), function(snapshot) {
         snapshot.forEach(function(ChildSnapshot) {
             //var keyDate = ChildSnapshot.key.substring(0,10); // ChildSnapshot.key = keys from 'Database/Date/'
-            var buttonNr = "button" + ChildSnapshot.key.substring(5);
+            var buttonNr = "button" + ChildSnapshot.key.substring(4);
             var bookerInitials = ChildSnapshot.val().Booker.split(" "); // ChildSnapshot.val().Booker = 'Booker' field value from keys
-            //var deskNr = ChildSnapshot.key.substring(5);
+            //var deskNr = ChildSnapshot.key.substring(15);
             //console.log(ChildSnapshot.key + ' - ' + ChildSnapshot.val().Booker);
             console.log(buttonNr, bookerInitials[0].substring(0,1)+ bookerInitials[1].substring(0,1));
             //if (keyDate == document.getElementById("datepicker").value) {
             //    document.getElementById(buttonNr).innerText = bookerInitials[0].substring(0,1) + bookerInitials[1].substring(0,1);
-            //    document.getElementById(buttonNr).style.backgroundColor = 'red';
+                document.getElementById(buttonNr).style.backgroundColor = 'red';
             //}
         })
     });
